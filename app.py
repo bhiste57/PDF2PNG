1ddae3d72fc392758914df72438eb55c5250c32d
Brian Histe <brian.histe@mnd.com>
2025-05-28 21:40:23 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,42 @@
+import os
+from flask import Flask, request, send_file, render_template
+from pdf2image import convert_from_bytes
+from io import BytesIO
+
+app = Flask(__name__)
+
+# Indique ici le chemin vers ton dossier poppler/bin
+POPPLER_PATH = r"C:\Users\bhiste\Release-24.08.0-0\poppler-24.08.0\Library\bin"
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/upload', methods=['POST'])
+def upload():
+    pdf_file = request.files['pdf_file']
+    pdf_bytes = pdf_file.read()
+
+    # Conversion avec le chemin vers poppler
+    images = convert_from_bytes(
+        pdf_bytes,
+        dpi=300,
+        first_page=1,
+        last_page=1,
+        poppler_path=POPPLER_PATH
+    )
+
+    img_io = BytesIO()
+    images[0].save(img_io, format='PNG')
+    img_io.seek(0)
+
+    return send_file(
+        img_io,
+        mimetype='image/png',
+        as_attachment=True,
+        download_name='page1.png'
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
